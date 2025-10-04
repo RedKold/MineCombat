db2d9ee3b70ed7037084f77d9109175efd477665
Kold <redkold233@gmail.com>
2025-10-04 21:48:57 +0800

    before turn to main

---
+++ Assets/_Scripts/Views/CardView.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private TMP_Text title;
+
+    [SerializeField] private TMP_Text description;
+
+    [SerializeField] private GameObject wrapper;
+
+    [SerializeField] private SpriteRenderer cost; // display the cost of the card
+
+    [SerializeField] private SpriteRenderer image; // display the image of the card
+
+
+}
